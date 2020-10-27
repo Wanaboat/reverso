2ee7d3a3c5650e6f2ab740c64a060ab01970e881
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2020-10-27 14:59:26 +0100

    Manage image

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,68 +3,90 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import buildSlug from '../utils/buildSlug'
 import Breadcrumbs from '../components/Breadcrumbs'
+import Img from "gatsby-image"
+
 import {
-    Box,
-    Flex,
-    Text,
-    Heading
+  Box,
+  Flex,
+  Text,
+  Heading,
+  SimpleGrid
 } from '@chakra-ui/core'
 
 const PageTpl = (props) => {
-    return (
-        <Layout lang={props.pageContext.lang}>
-            <Helmet>
-                <title>{props.data.prismicPage.data.seo_title}</title>
-                <meta name='description' content={ props.data.prismicPage.data.seo_description } />
-                <link
-                    rel='canonical'
-                    href={ `${process.env.BASE_URL}/${ buildSlug( props.data.prismicPage, props.pageContext.lang ) }` }
+  return (
+    <Layout lang={props.pageContext.lang}>
+      <Helmet>
+        <title>{props.data.prismicPage.data.seo_title}</title>
+        <meta name='description' content={props.data.prismicPage.data.seo_description} />
+        <link
+          rel='canonical'
+          href={`${process.env.BASE_URL}/${buildSlug(props.data.prismicPage, props.pageContext.lang)}`}
+        />
+        {props.data.prismicPage.alternate_languages[0] ?
+          <link
+            rel="alternate"
+            href={`${process.env.BASE_URL}/${buildSlug(props.data.prismicPage.alternate_languages[0], props.pageContext.lang)}`}
+            hreflang="x-default"
+          />
+          : null}
+      </Helmet>
+      <Breadcrumbs node={props.data.prismicPage} />
+      <Flex
+        minH='100vh'
+        justify='center'
+        alignItems='center'
+        color='gray.400'
+      >
+        <Box>
+          <Heading as='h1'
+            color='brand.secondary'
+            bg='brand.primary'
+          >
+            H1 : {props.data.prismicPage.data.title.text}
+          </Heading>
+          <Text>
+            Slug : {buildSlug(props.data.prismicPage)}
+          </Text>
+          <Text>
+            Base URL : {process.env.BASE_URL}
+          </Text>
+          <Text>
+            H1 : {props.data.prismicPage.data.title.text}
+          </Text>
+          <Text>
+            Seo title : {props.data.prismicPage.data.seo_title}
+          </Text>
+          <Text>
+            Seo description : {props.data.prismicPage.data.seo_description}
+          </Text>
+          <Text>
+            Alternate language : {buildSlug(props.data.prismicPage.alternate_languages[0], props.pageContext.lang)}
+          </Text>
+          {props.data.prismicPage.data.image.localFile ?
+            <Box>
+              <SimpleGrid columns='2'>
+                <picture>
+                  <source type='image/webp' srcSet={props.data.prismicPage.data.image.localFile.childImageSharp.fixed.srcSetWebp} />
+                  <source type='image/jpeg' srcSet={props.data.prismicPage.data.image.localFile.childImageSharp.fixed.srcSet} />
+                  <img loading='lazy' src={props.data.prismicPage.data.image.localFile.childImageSharp.original.src} />
+                </picture>
+                <Img
+                  fixed={props.data.prismicPage.data.image.localFile.childImageSharp.fixed}
+                  alt=""
+                  w='400px'
+                  h='400px'
                 />
-                { props.data.prismicPage.alternate_languages[0] ? 
-                <link
-                    rel="alternate"
-                    href={ `${process.env.BASE_URL}/${ buildSlug( props.data.prismicPage.alternate_languages[0], props.pageContext.lang ) }` }
-                    hreflang="x-default"
-                />
-                : null}
-            </Helmet>
-            <Breadcrumbs node={ props.data.prismicPage } />
-            <Flex
-                minH='100vh'
-                justify='center'
-                alignItems='center'
-                color='gray.400'
-            >
-                <Box>
-                <Heading as='h1'
-                  color='brand.secondary'
-                  bg='brand.primary'
-                >
-                    H1 : { props.data.prismicPage.data.title.text }
-                </Heading>
-                <Text>
-                    Slug : { buildSlug( props.data.prismicPage ) }
-                </Text>
-                <Text>
-                    Base URL : { process.env.BASE_URL }
-                </Text>
-                <Text>
-                    H1 : { props.data.prismicPage.data.title.text }
-                </Text>
-                <Text>
-                    Seo title : { props.data.prismicPage.data.seo_title }
-                </Text>
-                <Text>
-                    Seo description : { props.data.prismicPage.data.seo_description }
-                </Text>
-                <Text>
-                    Alternate language : { buildSlug( props.data.prismicPage.alternate_languages[0], props.pageContext.lang ) }
-                </Text>
-                </Box>
-                
-            </Flex>
-        </Layout>
-    )
+              </SimpleGrid>
+
+
+            </Box>
+            : null}
+        </Box>
+
+      </Flex>
+    </Layout>
+  )
 }
 
 export default PageTpl
@@ -111,6 +133,40 @@ query pageQuery($prismicId: ID) {
           }
           seo_title
           seo_description
+
+            image {
+              alt
+              copyright
+              url
+              thumbnails
+              localFile {
+                
+                childImageSharp {
+                  original {
+                    src
+                  }
+                  fixed {
+                    base64
+              tracedSVG
+              aspectRatio
+              srcWebp
+              srcSetWebp
+              originalName
+              srcSet
+              height
+              src
+              width
+                  }
+                }
+              }
+            }
+            body {
+              html
+              text
+              raw
+            }
+          
+
           parent {
             document {
               ... on PrismicPage {
