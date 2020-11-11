d13b3b993da2429c0422c95948ffe7446fb7220f
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2020-11-11 10:52:22 +0100

    Remove Fauna from NL admin

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,6 @@
-import faunadb from 'faunadb';
-const client = new faunadb.Client({
-  secret: 'fnAD5mMGnXACAaZ07REwSRrKUf8L6xSCVyMsGccn'
-});
-const q = faunadb.query;
-export { client, q };
+// import faunadb from 'faunadb';
+// const client = new faunadb.Client({
+//   secret: 'fnAD5mMGnXACAaZ07REwSRrKUf8L6xSCVyMsGccn'
+// });
+// const q = faunadb.query;
+// export { client, q };
