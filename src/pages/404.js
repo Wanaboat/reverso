1259241ab8b1aa08fa156c3449e9f749cdbfd388
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2020-11-17 10:24:04 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,38 @@
 import React from "react"
-
+import { Link as GatsbyLink } from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {
+  Box,
+  Button,
+  Heading,
+  Flex
+} from '@chakra-ui/core'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Flex
+      alignItems='center'
+      justifyContent='center'
+      h='800px'
+    >
+      <Box
+        textAlign='center'
+      >
+        <Heading fontSize='16px' mb='1rem' as='h1'>404: Not Found</Heading>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Button
+          mt='.5rem'
+          variant='link'
+          color='gray.500'
+          fontWeight='400'
+          to='/'
+          as={ GatsbyLink }
+        >Go to homepage</Button>
+      </Box>
+
+    </Flex>
   </Layout>
 )
 
