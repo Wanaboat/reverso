74b4977d7e66bdf9be4226dc7ca84d0c51dd9e94
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2020-12-03 17:12:20 +0100

    3d Reverso without button load

--- src/components/Reverso3d.js
+++ src/components/Reverso3d.js
@@ -10,54 +10,51 @@ import { FormattedMessage } from 'react-intl'
 const Reverso3d = () => {
     const [showPlayer, setShowPlayer] = useState(false)
     return (
-        !showPlayer ?
-            <Flex
-                minH={{ xs: '200px', lg: '500px' }}
-                alignItems='center'
-                justifyContent='center'
-                bg='brand.3'
-                id='3d'
+            // <Flex
+            //     minH={{ xs: '200px', lg: '500px' }}
+            //     alignItems='center'
+            //     justifyContent='center'
+            //     bg='brand.3'
+            //     id='3d'
+            // >
+            //     <BtnPrimary
+            //         handleClick={() => { setShowPlayer(true) }}
+            //     >
+            //         <FormattedMessage id='discover.3d' />
+            //     </BtnPrimary>
+            // </Flex>
+        <>
+            <AspectRatioBox
+                display={{ xs: 'block', lg: 'none' }}
+                maxW="100%"
+                w='100%'
+                // ratio={{ xs:1.77, lg: }}
+                ratio={0.7}
             >
-                <BtnPrimary
-                    handleClick={() => { setShowPlayer(true) }}
-                >
-                    <FormattedMessage id='discover.3d' />
-                </BtnPrimary>
-            </Flex>
-            :
-            <>
-                <AspectRatioBox
-                    display={{ xs: 'block', lg: 'none' }}
-                    maxW="100%"
-                    w='100%'
-                    // ratio={{ xs:1.77, lg: }}
-                    ratio={0.7}
-                >
-                    <Box
-                        as="iframe"
-                        title="Reverso 3D"
-                        src='https://emersya.com/showcase/85N0OLMVM4'
-                        allowFullScreen
-                    />
-                </AspectRatioBox>
-                <AspectRatioBox
-                    display={{ xs: 'none', lg: 'block' }}
-                    maxW="100%"
-                    w='100%'
-                    // ratio={{ xs:1.77, lg: }}
-                    ratio={1.6}
-                >
-                    <Box
-                        as="iframe"
-                        title="Reverso 3D"
-                        src='https://emersya.com/showcase/85N0OLMVM4'
-                        allowFullScreen
-                        frameborder={ 0 }
-                        allow='camera; gyroscope; accelerometer; magnetometer;'
-                    />
-                </AspectRatioBox>
-            </>
-
+                <Box
+                    as="iframe"
+                    title="Reverso 3D"
+                    src='https://emersya.com/showcase/85N0OLMVM4'
+                    allowFullScreen
+                />
+            </AspectRatioBox>
+            <AspectRatioBox
+                display={{ xs: 'none', lg: 'block' }}
+                maxW="100%"
+                w='100%'
+                // ratio={{ xs:1.77, lg: }}
+                ratio={1.6}
+            >
+                <Box
+                    as="iframe"
+                    title="Reverso 3D"
+                    src='https://emersya.com/showcase/85N0OLMVM4'
+                    allowFullScreen
+                    frameborder={ 0 }
+                    allow='camera; gyroscope; accelerometer; magnetometer;'
+                />
+            </AspectRatioBox>
+        </>
     )
 }
 
