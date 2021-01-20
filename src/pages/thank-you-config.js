a8b0bc2433e540212c3e5aa151c716c80645829b
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-01-20 07:37:58 +0100

    FIne tuning thank-you page

--- src/pages/thank-you-config.js
+++ src/pages/thank-you-config.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React from "react"
+import { Link as GatsbyLink } from 'gatsby'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 import {
-    Box,
-    Flex,
-    CSSReset,
-    ThemeProvider
-  } from '@chakra-ui/core'
-  import theme from '../theme'
+  Box,
+  Button,
+  Heading,
+  Flex
+} from '@chakra-ui/core'
 
 const ConfigThankyouPage = () => {
     return(
-        <ThemeProvider theme={ theme }>
-            <CSSReset />
-            <Flex
-                justify='center'
-                alignItems='center'
-            >
-                Thank-you, you'll receive soon all the details about your inquiry.
-            </Flex>
-        </ThemeProvider>
+        <Layout>
+        <SEO title="404: Not found" />
+        <Flex
+          alignItems='center'
+          justifyContent='center'
+          h='800px'
+        >
+          <Box
+            textAlign='center'
+          >
+            <Heading fontSize='16px' mb='1rem' as='h1'>Congratulations, your config is complete.</Heading>
+            <p>You'l receive soon all the details by email.</p>
+            <Button
+              mt='.5rem'
+              variant='link'
+              color='gray.500'
+              fontWeight='400'
+              to='/'
+              as={ GatsbyLink }
+            >Go to homepage</Button>
+          </Box>
+    
+        </Flex>
+      </Layout>
     )
 }
 
