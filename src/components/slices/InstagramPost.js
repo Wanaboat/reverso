a42b61dcd8a15300ae27fb9f5d7410459fe48920
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-02-26 11:52:47 +0100

    Slice Instagram v1

--- src/components/slices/InstagramPost.js
+++ src/components/slices/InstagramPost.js
@@ -1,21 +1,20 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import {
     Box,
     Flex
 } from '@chakra-ui/core'
 
+
 const InstagramPost = ( props ) => {
     return(
         <Flex justifyContent='center' py={{ xs:2, lg:4 }}>
-            {/* InstagramPost
-            <pre>
-                { JSON.stringify( props, null, 2 )}
-            </pre> */}
+            <Helmet>
+                <script async src="https://www.instagram.com/embed.js" />
+            </Helmet>
             <Box w='100%' maxW='42rem'>
-            <div dangerouslySetInnerHTML={{ __html: props.data.instagram_post.html }}></div>
-
+                <div dangerouslySetInnerHTML={{ __html: props.data.instagram_post.html }}></div>
             </Box>
-
         </Flex>
     )
 }
