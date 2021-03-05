3e755c308cae776bd92a87d7e4976e2d439f5b83
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-03-05 10:59:04 +0100

    Translations fix

--- src/templates/page.js
+++ src/templates/page.js
@@ -56,7 +56,7 @@ const PageTpl = (props) => {
           <meta name='og:image' content={`${process.env.GATSBY_BASE_URL}${props.data.prismicPage.data.sharing_image.localFile.childImageSharp.fixed.src}`} />
           : null : null}
       </Helmet>
-      <pre>
+      {/* <pre>
         { JSON.stringify( data.sisters, null, 2 )}
       </pre>
       <pre>
@@ -64,7 +64,7 @@ const PageTpl = (props) => {
       </pre>
       <pre>
         { JSON.stringify( props.pageContext.lang, null, 2 )}
-      </pre>
+      </pre> */}
       { data.prismicPage.data.display_children_sticky_nav ? <StickyChildrenNav childrenPages={data.children.edges} /> : null }
       <Wrapper
         bg='white'
